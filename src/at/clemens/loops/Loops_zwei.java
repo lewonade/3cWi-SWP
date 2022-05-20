d4032eb55ced1a16859614f2efaa06d123ed341f
Lewonade <clemensdancso@gmail.com>
2022-05-20 12:08:20 +0200

    I have a problem to execute my main, just trying to look if the issue is that i havent't commited the last changes

--- src/at/clemens/loops/Loops_zwei.java
+++ src/at/clemens/loops/Loops_zwei.java
@@ -1,7 +1,7 @@
 package at.clemens.loops;
 
 public class Loops_zwei {
-    private static int test(int bisZahl) {
+    public static int main(String[] args) {
 
         int zwischenergebnis = 0;
         for (int i = 1; i <= 1000; i++) {
